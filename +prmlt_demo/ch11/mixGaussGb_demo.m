695df9ba74dce4da894d1125458c8c32b8c7cc84
Ezequiel Juarez Garcia <ezequieljuarezg@gmail.com>
2024-06-12 17:24:41 -0400

    Fix ch 11 mixture of Gaussians demo

--- +prmlt_demo/ch11/mixGaussGb_demo.m
+++ +prmlt_demo/ch11/mixGaussGb_demo.m
@@ -1,16 +1,20 @@
 %% Collapse Gibbs sampling for Dirichelt process gaussian mixture model
-close all; clear;
+close all;
+clear;
+rng(38);
 d = 2;
 k = 3;
 n = 500;
-[X,z] = mixGaussRnd(d,k,n);
-plotClass(X,z);
+[X, z] = prmlt_chapter09.mixGaussRnd(d, k, n);
+figure;
+prmlt_common.plotClass(X, z);
 
-[z,Theta,w,llh] = mixGaussGb(X);
-figure
-plotClass(X,z);
+%% Training
+[z, Theta, w, llh] = prmlt_chapter11.mixGaussGb(X);
+figure;
+prmlt_common.plotClass(X, z);
 
-[X,z] = mixGaussSample(Theta,w,n);
-figure
-plotClass(X,z);
-
+%% Testing
+[X2, z2] = prmlt_chapter11.mixGaussSample(Theta, w, n);
+figure;
+prmlt_common.plotClass(X2, z2);
